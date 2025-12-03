39d3172deca541c60db4edd1cd9acd6c142ad868
Alexandre Angrignon <alex040892@gmail.com>
2025-12-03 22:37:10 +1300

    Configuring entities for migration and connecting database to sql server object explorer

---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2");
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -7,5 +8,13 @@ namespace Infrastructure.Data;
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+
+    }
 }
 
